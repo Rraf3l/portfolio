1c223dc659b9a5d66de766612fa9249dd171799f
Rraf3l <127207161+Rraf3l@users.noreply.github.com>
2024-04-03 12:36:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function Insta(){
+    window.open("https://www.instagram.com/raf.schmidt/");
+}
+function Linkedin(){
+    window.open("https://www.linkedin.com/in/rafael-dos-santos-schmidt-335228269/?originalSubdomain=br");
+}
+function Github(){
+    window.open("https://github.com/Rraf3l");
+}
